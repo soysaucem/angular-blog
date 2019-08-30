654aa0fab98ca8ca73d005ac61df3b05690dbc8c
Triet Ong <trietjack@gmail.com>
2019-08-30 17:48:49 +1000

    remove default e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,10 +8,10 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-blog!');
-  });
+  // it('should display welcome message', () => {
+  //   page.navigateTo();
+  //   expect(page.getTitleText()).toEqual('Welcome to angular-blog!');
+  // });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
